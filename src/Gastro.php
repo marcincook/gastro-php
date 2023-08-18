4211cb99651bfc34e4f3689648bec18a80875102
marcincook <marcincook@users.noreply.github.com>
2023-08-18 00:21:13 +0000

    Fix styling

--- src/Gastro.php
+++ src/Gastro.php
@@ -4,17 +4,12 @@ namespace Marcincook\Gastro;
 
 class Gastro
 {
-
     public array $tables;
 
-    /**
-     * @param array $tables
-     * @return Gastro
-     */
     public function setTables(array $tables): Gastro
     {
         $this->tables = $tables;
+
         return $this;
     }
-
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,13 +1,11 @@
 <?php
 
-
-
 it('can test', function () {
     expect(true)->toBeTrue();
 });
 
-it('tables', function (){
-    $tables = ['towar','cena'];
+it('tables', function () {
+    $tables = ['towar', 'cena'];
     $gastro = new Marcincook\Gastro\Gastro();
     $gastro->setTables($tables);
 
